692e73b98032188c557c1e4fa90c473f52a89fee
Ajay Kumar <64101601+asalwania@users.noreply.github.com>
2026-04-05 15:56:18 +0530

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s, k) {
+        let l=0, r=0, res=0, maxf = 0;
+
+        const freq = new Array(26).fill((0));
+        const charCodeOfA = "A".charCodeAt(0); 
+        
+        while(r < s.length){
+           freq[s[r].charCodeAt(0) - charCodeOfA]++;
+           maxf = Math.max(maxf, freq[s[r].charCodeAt(0) - charCodeOfA]);
+
+           while((r-l+1 -maxf) > k){
+                freq[s[l].charCodeAt(0) - charCodeOfA]--;
+                l++;
+           }
+           res = Math.max(res, r-l+1);
+           r++;
+        }
+        return res;
+    }
+}
